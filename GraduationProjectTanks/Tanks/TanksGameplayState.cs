efac6a930d8d4cd840ce09b48d9887c111103849
mirikol <kirilloffk@gmail.com>
2025-11-11 08:55:37 +0500

    AddClass

---
+++ GraduationProjectTanks/Tanks/TanksGameplayState.cs
@@ -0,0 +1,27 @@
+﻿using GraduationProjectTanks.Shared;
+
+namespace GraduationProjectTanks.Tanks
+{
+    public class TanksGameplayState : BaseGameState
+    {
+        public override void Update(float deltaTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(ConsoleRenderer renderer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsDone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
